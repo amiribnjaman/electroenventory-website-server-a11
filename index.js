5907468a6d83d2da3fd2424336ea64d36a1d4329
amiribnjaman <amiribnjaman@gmail.com>
2022-05-01 19:48:27 +0600

    Middlwares added

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express()
 const port = process.env.PORT || 4000
+const cors = require('cors')
+require('dotenv').config()
+
+// Middlewares
+app.use(cors())
+app.use(express.json())
 
 
+// Mongodb connection
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.urqpl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 client.connect(err => {
     const collection = client.db("test").collection("devices");
     // perform actions on the collection object
