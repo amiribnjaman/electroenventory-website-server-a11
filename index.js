43df5cf8b53ec0e06473ce6dfc340f3ba48e3558
amiribnjaman <amiribnjaman@gmail.com>
2022-05-01 20:06:16 +0600

    Post api implemented

--- index.js
+++ index.js
@@ -14,12 +14,32 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.urqpl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// mongo run function
+(async () => {
+    try {
+        await client.connect()
+        const inventoryCollection = client.db('electronicsInventory').collection('inventories')
 
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+        app.post('/inventory', async (req, res) => {
+            const data = req.body
+            console.log(data);
+            const result = await inventoryCollection.insertOne(data)
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+})().catch(console.dir)
+
+// {
+//  "name":"iphone",
+//  "image":"https://i.ibb.co/KsBY2Q2/trainer2.jpg",
+//   "desc":"this is description",
+//   "price":"200",
+//   "quentity":"2",
+//   "spplier_name":"zihad"
+// }
 
 
 
