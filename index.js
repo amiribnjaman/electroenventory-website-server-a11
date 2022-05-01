95fd5c4da02e00edc8e587f230613acdbf956996
amiribnjaman <amiribnjaman@gmail.com>
2022-05-01 19:42:03 +0600

    Mongodb database created and  connected

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.urqpl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Server ok')
 })
+
+
+
 
 app.listen(port, () => {
     console.log('Server running on port', port);
