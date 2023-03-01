138d7a06d3220d718c94a8f586c26701785fca75
amiribnjaman <amiribnjaman@gmail.com>
2023-03-01 22:02:35 +0600

    testing

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ require('dotenv').config()
 const jwt = require('jsonwebtoken');
 
 // Middlewares
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 
+// const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) 
 
 // Mongodb connection
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
