6afaced19f0c50bebce5401a3d8a2b3bc21e7272
amiribnjaman <amiribnjaman@gmail.com>
2023-03-02 18:51:28 +0600

    Vercel re created

--- index.js
+++ index.js
@@ -37,6 +37,8 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.urqpl.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+
 // mongo run function
 (async () => {
     try {
