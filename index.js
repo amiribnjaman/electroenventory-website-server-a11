26e097d8fecd8afbc3b92a89ee35c9df33430800
amiribnjaman <amiribnjaman@gmail.com>
2023-05-26 20:26:53 +0600

    Vercel testing

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
             const sort = { length: -1 }
             const limit = 6
             const cursor = inventoryCollection.find(query).sort(sort).limit(limit)
-            const result = cursor.toArray()
+            const result = await cursor.toArray()
 
             res.send(result)
         })
